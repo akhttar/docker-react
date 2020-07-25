19b242a4a8edb6dfd2edda796fa7251fd62dda1d
akhttar <akhttar4.0@gmail.com>
2020-07-25 12:29:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,4 +15,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 
 // Learn more about service workers: https://bit.ly/CRA- changed fb_5 version new changes
+//Rebase on master branch
 serviceWorker.unregister();
